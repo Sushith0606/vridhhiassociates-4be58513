9387270beb306d117e952b6fd62b589393aa5d89
Sushith0606 <46609459+Sushith0606@users.noreply.github.com>
2026-02-03 17:32:38 -0500

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { Menu, X, Phone, Mail } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { motion, AnimatePresence } from "framer-motion";
-import logo from "@/assets/Vridhhi.jpg";
+import logo from "@/assets/Vridhhip.png";
 
 const navLinks = [
   { name: "Home", href: "/" },
